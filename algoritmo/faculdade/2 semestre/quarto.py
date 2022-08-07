ce63f125a5e0935f0010e8b4d88869bc83e2309b
Vitor Cunha <vitor84395798@gmail.com>
2022-08-07 17:17:30 -0300

    exercicios de tupla

---
+++ algoritmo/faculdade/2 semestre/quarto.py
@@ -0,0 +1,2 @@
+# Crie um programa que vai gerar dez números aleatórios e colocarem uma tupla. Depois disso mostre a listagem de númerosgerados e também indique o menor e o maior valor que estão natupla.
+
