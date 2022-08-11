f2768c830eed752e53f9cc1b92f47d0d28252ebf
vitor cunha <vitor84395798@gmail.com>
2022-08-11 19:44:29 -0300

    prrimeira atividade de lista

---
+++ algoritmo/faculdade/2 semestre/lista.py
@@ -0,0 +1,30 @@
+# lista vazia
+
+listavazia = []
+
+listavazia2 = list()
+
+listaalunos = ["maariaa", "carinha", "tropa"]
+
+print(listaalunos[2])
+
+cont = 0
+
+while cont <= len (listaalunos):
+    print(f"{listaalunos[cont]}")
+    cont += 1
+
+# adicinando termos a lista
+listaalunos.append("pedro")
+listaalunos>append(input("informe o nome do aluno"))
+
+listaalunos.insert(1, "eduardo")
+
+# remover nome da lista
+
+listaalunos.remove(input())
+
+for i in range(len(listaalunos)):
+    print(f"{listaalunos[i]}")
+
+
