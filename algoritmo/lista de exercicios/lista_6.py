b0a6f4de3da0108f511d26e39683cc29348a9066
Vitor Cunha <vitor84395798@gmail.com>
2022-08-14 18:44:27 -0300

    ajustes de codigo

--- algoritmo/lista de exercicios/lista_6.py
+++ algoritmo/lista de exercicios/lista_6.py
@@ -1,14 +1,4 @@
 # Faça um Programa que peça as quatro notas de 10 alunos, calcule e armazene num vetor a média de cada aluno, imprima o número de alunos com média maior ou igual a 7.0.
-# aluno1 = list(float(input("insira as notas do aluno " )) for x in range(4))
-# aluno2 = list(float(input("insira as notas do aluno " )) for x in range(4))
-# aluno3 = list(float(input("insira as notas do aluno " )) for x in range(4))
-# aluno4 = list(float(input("insira as notas do aluno " )) for x in range(4))
-# aluno5 = list(float(input("insira as notas do aluno " )) for x in range(4))
-# aluno6 = list(float(input("insira as notas do aluno " )) for x in range(4))
-# aluno7 = list(float(input("insira as notas do aluno " )) for x in range(4))
-# aluno8 = list(float(input("insira as notas do aluno " )) for x in range(4))
-# aluno9 = list(float(input("insira as notas do aluno " )) for x in range(4))
-# aluno10 = list(float(input("insira as notas do aluno " )) for x in range(4))
 
 alunos = list()
 
