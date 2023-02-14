d880e06def7b566f7412c622cc5da0f87b0d17ea
Vitor Cunha <104070767+Vitorcalves@users.noreply.github.com>
2023-02-14 19:56:12 -0300

    testando java

---
+++ java/PrimeiroPrograma/src/primeiroprograma/PrimeiroPrograma.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package primeiroprograma;
+
+/**
+ *
+ * @author vitor
+ */
+public class PrimeiroPrograma {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("Ola, mundo");
+        System.out.println("Ola, 2 mundo");
+    }
+    
+}
---
+++ java/calculos/src/matematica/operacaoAritimetica.java
@@ -0,0 +1,12 @@
+
+package matematica;
+
+
+public class operacaoAritimetica {
+    
+    //tributos da classe operacaAritimetica
+    protected float operadorUm;
+    protected float operadorUm;
+    
+    
+}
