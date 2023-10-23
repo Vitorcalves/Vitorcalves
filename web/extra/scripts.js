0e764ba6a8e3aab407f2de1495be825b06792ccb
vitor <104070767+Vitorcalves@users.noreply.github.com>
2023-10-23 04:58:26 -0300

    funcao trocar nome e placar

--- web/extra/scripts.js
+++ web/extra/scripts.js
@@ -1,32 +1,59 @@
-var jogadorX = [];
-var jogadorO = [];
-var rodada = 0;
+class Jogador {
+    constructor(nome, jogadas, vitorias) {
+        this.nome = nome;
+        this.jogadas = jogadas;
+        this.vitorias = vitorias;
+    }
+    ganhar(){
+        this.vitorias++;
+    }
+    jogar(jogada){
+        this.jogadas.push(jogada);
+    }
+}
+
+let X = new Jogador("X", [], 0);
+let O = new Jogador("O", [], 0);
 var ganhador = false;
-const idDiv = document.getElementById('alert')
+var rodada = 0;
+const idDiv = document.getElementById('alert');
+const btn = document.querySelector("#send");
+
+btn.addEventListener('click', function(e) {
+    e.preventDefault();
+    const nome = document.querySelector("#nome01");
+    const nome02 = document.querySelector("#nome02");
+    X.nome = nome.value;
+    O.nome = nome02.value;
+    document.getElementById('jogador1').innerText = X["nome"];
+    document.getElementById('jogador2').innerText = O["nome"];
+
+});
+
 // Função para aceitar o clique do botão
 function clique(botao) {
     let id = +botao.id;
     console.log(id);
     // Verificar se o botão já foi clicado ou se já tem um ganhador
-    for (let i = 0; i < jogadorX.length; i++) {
-        if (jogadorX[i] == id || jogadorO[i] == id || ganhador == true) {
+    for (let i = 0; i < X.jogadas.length; i++) {
+        if (X.jogadas[i] == id || O.jogadas[i] == id || ganhador == true) {
             return;
         }
     }
     if (rodada == 0) {
         document.getElementById(id).innerText = "X";
-        jogadorX.push(id);
+        X.jogar(id);
         rodada++;
     } else if (rodada % 2 == 0) {
         document.getElementById(id).innerText = "X";
-        jogadorX.push(id);
+        X.jogar(id);
         rodada++;
-        verificarVencedor("X");
+        verificarVencedor(X);
     } else {
         document.getElementById(id).innerText = "O";
-        jogadorO.push(id);
+        O.jogar(id);
         rodada++;
-        verificarVencedor("O");
+        verificarVencedor(O);
     }
 }
 // Função para verificar se houve um vencedor
@@ -44,17 +71,19 @@ function verificarVencedor(jogador) {
     ];
     // Percorre as combinações e compara com os valores
     if (rodada >= 5){
-        if(jogador == "X"){
-            correrLista(jogadorX);
+        if(jogador == X){
+            correrLista(X.jogadas);
         }else{
-            correrLista(jogadorO);
+            correrLista(O.jogadas);
         }
     }
     function correrLista(lista){
         for (let i = 0; i < comb.length; i++) {
             if (lista.includes(comb[i][0]) && lista.includes(comb[i][1]) && lista.includes(comb[i][2])) {  
                 ganhador = true;
-                mensagemAlerta(`O Jogador ${jogador} Venceu!`);
+                mensagemAlerta(`O Jogador ${jogador.nome} Venceu!`);
+                jogador.ganhar();
+                console.log(jogador.vitorias);
                 break;
             }
         }
@@ -66,12 +95,28 @@ function verificarVencedor(jogador) {
         mensagemAlerta("O jogo terminou em empate!");
     }
 }
+function novaRodada(){
+    rodada = 0;
+    ganhador = false;
+    X.jogadas = [];
+    O.jogadas = [];
+        document.getElementById('placar1').innerText = X.vitorias;
+        document.getElementById('placar2').innerText = O.vitorias;
+        idDiv.innerHTML = '';
+
+    for(let i = 0; i < 9; i++){
+        document.getElementById(i).innerText = "";
+    }
+}
+
 // Função para mostrar uma mensagem de vitoria ou empate
 function mensagemAlerta(message) {
     const escrever = document.createElement('div');
-    escrever.innerHTML = [
-        `<div class="mensagem">`,
+    escrever.innerHTML = [       
+        
+        '<div class="mensagem">',
         `   <div>${message}</div>`,
+        '   <button class="close" onclick="novaRodada();">Nova Rodada</button>',
         '   <button class="close" onclick="window.location.reload();">Reiniciar</button>',
         '</div>'
     ].join('');
